97ec689942ab5066bd9220769c65868f1fea864f
Thiago Zanetti <thiago.project@gmail.com>
2016-10-13 18:01:41 -0400

    Fixing rvm info script

--- rvm-custom.zsh
+++ rvm-custom.zsh
@@ -1,20 +1,36 @@
 function rvm_prompt_info {
 
-local rvm_ruby=""
+  [ -f "Gemfile" ] || [ -f ".ruby-{version,gemset}" ] || return
 
-if ${HOME}/.rvm/bin/rvm-prompt &> /dev/null; then # detect user-local rvm installation
-  rvm_ruby='$(${HOME}/.rvm/bin/rvm-prompt i v g s)'
-elif which rvm-prompt &> /dev/null; then # detect system-wide rvm installation
-  rvm_ruby='$(rvm-prompt i v g s)'
-elif which rbenv &> /dev/null; then # detect Simple Ruby Version Management
-  rvm_ruby='$(rbenv version | sed -e "s/ (set.*$//")'
-fi
+  local rb rb_version gem_set rvm_version
 
-if [[ "${rvmruby}x" == "x" ]]; then
-  echo ""
-  return
-fi
+  if ${HOME}/.rvm/bin/rvm-prompt &> /dev/null; then # detect user-local rvm installation
+    rb=$(${HOME}/.rvm/bin/rvm-prompt i v g s)
+  elif which rvm-prompt &> /dev/null; then # detect system-wide rvm installation
+    rb='$(rvm-prompt i v g s)'
+  elif which rbenv &> /dev/null; then # detect Simple Ruby Version Management
+    rb='$(rbenv version | sed -e "s/ (set.*$//")'
+  fi
 
-echo "$ZSH_THEME_RVM_PROMPT_PREFIX${rvm_ruby}$ZSH_THEME_RVM_PROMPT_SUFFIX"
+  if ${HOME}/.rvm/bin/rvm &> /dev/null; then
+    rvm_version=${$(${HOME}/.rvm/bin/rvm --version 2> /dev/null)[(ws: :)2]}
+  fi
 
+  if [[ "${rb}x" == "x" ]]; then
+    echo ""
+    return
+  fi
+
+  rb_version=${rb[(ws:@:)1][(ws:-:)2]}
+  gem_set=${rb[(ws:@:)2]}
+
+  if [[ "${gem_set}x" != "x" ]]; then
+    rb_version=${rb_version}@${gem_set}
+  fi
+
+  if [[ "${rvm_version}x" != "x" ]]; then
+    rb_version=rvm:${rvm_version}"|"rb:${rb_version}
+  fi
+
+  echo "$ZSH_THEME_RVM_PROMPT_PREFIX${rb_version}$ZSH_THEME_RVM_PROMPT_SUFFIX"
 }
