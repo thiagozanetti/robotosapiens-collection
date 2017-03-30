02ff9ebc85d090bab010e72083328ef3cd628586
Thiago Zanetti <thiago.project@gmail.com>
2017-03-30 16:38:40 -0400

    Improving validation for gulp bower typings and typescript

--- nvm-custom.zsh
+++ nvm-custom.zsh
@@ -1,14 +1,10 @@
 function nvm_prompt_info {
   [ -f "$HOME/.nvm/nvm.sh" ] || return
   local nvm_prompt nvm_v node_v npm_v bower_v gulp_v typings_v ts_v
-  
+
   nvm_v=$(nvm --version 2>/dev/null)
   node_v=$(node -v 2>/dev/null)
   npm_v=$(npm -v 2>/dev/null)
-  bower_v=$(bower -v 2>/dev/null)
-  gulp_v=$(gulp -v 2>/dev/null)
-  typings_v=$(typings -v 2>/dev/null)
-  ts_v=$(tsc -v 2>/dev/null)
 
   if [[ "${nvm_v}x" == "x" || "${node_v}x" == "x" || "${nvm_v}x" == "x" ]] ; then
     echo ""
@@ -17,20 +13,35 @@ function nvm_prompt_info {
 
   nvm_prompt="nvm:${nvm_v}|node:${node_v:1}|npm:${npm_v}"
 
-  if [[ "${bower_v}x" != "x" && -d "bower_components" ]] ; then
-    nvm_prompt="${nvm_prompt}|bower:${bower_v}"
+  if [[ -d "bower_components" ]] ; then
+    bower_v=$(bower -v 2>/dev/null)
+
+    if [[ "${bower_v}x" != "x"  ]] ;  then
+      nvm_prompt="${nvm_prompt}|bower:${bower_v}"
+    fi
   fi
 
-  if [[ "${gulp_v}x" != "x" ]] && ls gulpfile* &> /dev/null ; then
-    nvm_prompt="${nvm_prompt}|gulp:${(f)gulp_v[(ws: :)-1]}"
+  if ls gulpfile* &> /dev/null ; then
+    gulp_v=$(gulp -v 2>/dev/null)
+
+    if [[ "${gulp_v}x" != "x" ]] ; then
+      nvm_prompt="${nvm_prompt}|gulp:${(f)gulp_v[(ws: :)-1]}"
+    fi
   fi
 
-  if [[ "${typings_v}x" != "x" && -f "typings.json" ]] ; then
-    nvm_prompt="${nvm_prompt}|typings:${typings_v}"
+  if [[ -f "typings.json" ]] ; then
+    typings_v=$(typings -v 2>/dev/null)
+
+    if [[ "${typings_v}x" != "x" ]] ;  then
+      nvm_prompt="${nvm_prompt}|typings:${typings_v}"
+    fi
   fi
 
-  if [[ "${ts_v}x" != "x" && -f "tsconfig.json" ]] ; then
-    nvm_prompt="${nvm_prompt}|tsc:${ts_v[(ws: :)2]}"
+  if [[ -f "tsconfig.json" ]] ; then
+    ts_v=$(tsc -v 2>/dev/null)
+    if [[ "${ts_v}x" != "x" ]] ; then
+      nvm_prompt="${nvm_prompt}|tsc:${ts_v[(ws: :)2]}"
+    fi
   fi
 
   nvm_prompt="[${nvm_prompt}]"
