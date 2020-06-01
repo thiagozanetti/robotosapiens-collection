07eb25cafeb5a8d212882f3868b119ff9daa2890
Thiago Zanetti <zanetti@devgrid.co.uk>
2020-06-01 09:38:48 -0300

    Enable nvm prompt only when inside a js project to avoid too much information on ps1 line

--- nvm-custom.zsh
+++ nvm-custom.zsh
@@ -1,57 +1,59 @@
 function nvm_prompt_info {
-  setopt +o nomatch
+  if [[ -a $PWD/package.json  ]]; then
+    setopt +o nomatch
 
-  [ -f "$HOME/.nvm/nvm.sh" ] || return
-  local nvm_prompt nvm_v node_v npm_v bower_v gulp_v typings_v ts_v
+    [ -f "$HOME/.nvm/nvm.sh" ] || return
+    local nvm_prompt nvm_v node_v npm_v bower_v gulp_v typings_v ts_v
 
-  nvm_v=$(nvm --version 2>/dev/null)
-  node_v=$(node -v 2>/dev/null)
-  npm_v=$(npm -v 2>/dev/null)
-  yarn_v=$(yarn -v 2>/dev/null)
+    nvm_v=$(nvm --version 2>/dev/null)
+    node_v=$(node -v 2>/dev/null)
+    npm_v=$(npm -v 2>/dev/null)
+    yarn_v=$(yarn -v 2>/dev/null)
 
-  if [[ "${nvm_v}x" == "x" || "${node_v}x" == "x" || "${npm_v}x" == "x" ]] ; then
-    echo ""
-    return
-  fi
-
-  nvm_prompt="nvm:${nvm_v}|node:${node_v:1}|npm:${npm_v}"
-
-  if [[ "${yarn_v}x" != "x"  ]] ;  then
-    nvm_prompt="${nvm_prompt}|yarn:${yarn_v}"
-  fi
-
-  if [[ -d "bower_components" ]] ; then
-    bower_v=$(bower -v 2>/dev/null)
-
-    if [[ "${bower_v}x" != "x"  ]] ;  then
-      nvm_prompt="${nvm_prompt}|bower:${bower_v}"
+    if [[ "${nvm_v}x" == "x" || "${node_v}x" == "x" || "${npm_v}x" == "x" ]] ; then
+      echo ""
+      return
     fi
-  fi
 
-  if ls gulpfile* &> /dev/null ; then
-    gulp_v=$(gulp -v 2>/dev/null)
+    nvm_prompt="nvm:${nvm_v}|node:${node_v:1}|npm:${npm_v}"
 
-    if [[ "${gulp_v}x" != "x" ]] ; then
-      nvm_prompt="${nvm_prompt}|gulp:${(f)gulp_v[(ws: :)-1]}"
+    if [[ "${yarn_v}x" != "x"  ]] ;  then
+      nvm_prompt="${nvm_prompt}|yarn:${yarn_v}"
     fi
-  fi
 
-  if [[ -f "typings.json" ]] ; then
-    typings_v=$(typings -v 2>/dev/null)
+    if [[ -d "bower_components" ]] ; then
+      bower_v=$(bower -v 2>/dev/null)
 
-    if [[ "${typings_v}x" != "x" ]] ;  then
-      nvm_prompt="${nvm_prompt}|typings:${typings_v}"
+      if [[ "${bower_v}x" != "x"  ]] ;  then
+        nvm_prompt="${nvm_prompt}|bower:${bower_v}"
+      fi
     fi
-  fi
 
-  if [[ -f "tsconfig.json" ]] ; then
-    ts_v=$(tsc -v 2>/dev/null)
-    if [[ "${ts_v}x" != "x" ]] ; then
-      nvm_prompt="${nvm_prompt}|tsc:${ts_v[(ws: :)2]}"
+    if ls gulpfile* &> /dev/null ; then
+      gulp_v=$(gulp -v 2>/dev/null)
+
+      if [[ "${gulp_v}x" != "x" ]] ; then
+        nvm_prompt="${nvm_prompt}|gulp:${(f)gulp_v[(ws: :)-1]}"
+      fi
     fi
+
+    if [[ -f "typings.json" ]] ; then
+      typings_v=$(typings -v 2>/dev/null)
+
+      if [[ "${typings_v}x" != "x" ]] ;  then
+        nvm_prompt="${nvm_prompt}|typings:${typings_v}"
+      fi
+    fi
+
+    if [[ -f "tsconfig.json" ]] ; then
+      ts_v=$(tsc -v 2>/dev/null)
+      if [[ "${ts_v}x" != "x" ]] ; then
+        nvm_prompt="${nvm_prompt}|tsc:${ts_v[(ws: :)2]}"
+      fi
+    fi
+
+    nvm_prompt="${nvm_prompt}"
+
+    echo "${ZSH_THEME_NVM_PROMPT_PREFIX}${nvm_prompt}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
   fi
-
-  nvm_prompt="${nvm_prompt}"
-
-  echo "${ZSH_THEME_NVM_PROMPT_PREFIX}${nvm_prompt}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
 }
